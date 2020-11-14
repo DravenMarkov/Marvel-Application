825735cba59d8697a359d779b0ac0115eb3be244
Zenemij <ti.ericjimenez@gmail.com>
2020-11-14 14:38:41 +0100

    adding custom toolbar

--- app/src/main/java/com/example/marvelapplication/ui/base/BaseActivity.kt
+++ app/src/main/java/com/example/marvelapplication/ui/base/BaseActivity.kt
@@ -2,13 +2,16 @@ package com.example.marvelapplication.ui.base
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 abstract class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(getContentView())
+        setSupportActionBar(getMyActionBar())
     }
 
+    protected abstract fun getMyActionBar(): Toolbar
     protected abstract fun getContentView(): Int
 }
--- app/src/main/java/com/example/marvelapplication/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/marvelapplication/ui/main/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.example.marvelapplication.ui.main
 
+import androidx.appcompat.widget.Toolbar
 import com.example.marvelapplication.R
 import com.example.marvelapplication.ui.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
     override fun getContentView(): Int = R.layout.activity_main
 
-
+    override fun getMyActionBar(): Toolbar = toolbar
 
 
 }
